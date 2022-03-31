8c72d5c300f35431c79da6d85eb1f72c315f9dfc
Franco Rafael <frafael@advancedmankind.com>
2022-03-30 21:56:22 -0300

    ate a aula 218

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -1,3 +1,5 @@
+
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:lh_virtual_store/datas/product_data.dart';
 
@@ -9,16 +11,54 @@ class ProductScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.green,
-      child: Column(
-        children: [
-          Expanded(
-              child: Container(
-                  padding: EdgeInsets.all(4),
-                  child: Image.network(productData.images[0], fit: BoxFit.scaleDown,))),
-        ],
-      ),
+    return Scaffold(
+          appBar: AppBar(
+            title: Text(productData.title),
+            centerTitle: true,
+          ),
+          body: ListView(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: AspectRatio(
+                    aspectRatio: 1.5,
+                    child: CarouselSlider(
+                      options: CarouselOptions(
+                        initialPage: 0,
+                        enableInfiniteScroll: true,
+                        autoPlay: true,
+                          aspectRatio: 01
+                      ),
+                      items: productData.images.map((url){
+                        return Image.network(url.toString());
+                      }).toList(),
+                    ),
+                ),
+              ),
+              Padding(
+                  padding: EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(productData.title,
+                    style: const TextStyle(
+                      color: Colors.black54,
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    ),
+                    Text("R\$ ${productData.price.toStringAsFixed(2)}",
+                      style:  const TextStyle(
+                        color: Colors.blueAccent,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
     );
   }
 }
